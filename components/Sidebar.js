b4df832e9d2ddbeba38cc878ec824744889f039f
Pat  Pignataro <pat6284@msn.com>
2019-06-24 18:44:49 +0000

    Done.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,14 +3,34 @@
 const React = require('react');
 const SidebarItem = require('./SidebarItem');
 
-class Sidebar extends React.Component {
-  handleClick(index, ev) {
+class Sidebar extends React.Component { constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+   handleClick(index, e) {
+    e.preventDefault();
+    this.props.onSelect(index);
+  }
+
+   generateSidebarItems() {
+    const { files, selectedFileIndex } = this.props;
+    return files.map(
+      (file, index) =>
+        <SidebarItem
+          key={index}
+          isSelected={index === selectedFileIndex}
+          file={file}
+          onClick={this.handleClick.bind(null, index)}
+        />
+    );
   }
   render() {
     const { files, selectedFileIndex, onAdd } = this.props;
 
     return (
       <ul className="sidebar">
+      {this.generateSidebarItems()}
       </ul>
     );
   }
